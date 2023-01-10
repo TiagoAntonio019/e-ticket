15b32e0a15c38696f19d5e544246d7e197e21443
Tiago Moises Antonio <tiagoantonio019@gmail.com>
2023-01-10 19:21:47 -0300

    Closing procedure created

--- eticket.sql
+++ eticket.sql
@@ -42,12 +42,48 @@ CREATE TABLE `ticket` (
   `entry_date` datetime NOT NULL,
   `closing_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping routines for database 'eticket'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `close_ticket` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `close_ticket`(IN ticket_id int)
+label1:BEGIN
+    declare flag datetime;
+    declare ticket_entry_date datetime;
+    declare msg_text varchar(50);
+    
+    set ticket_entry_date = (select entry_date from ticket where id=ticket_id);
+    if(ticket_entry_date is null) then
+        select "Ticket não existe" as "msg";
+        leave  label1;
+    end if;
+    
+    set flag = (select closing_date from ticket where id= ticket_id);
+    if(flag is null) then
+        update ticket set closing_date = now() where id= ticket_id;
+        set msg_text = "Ticket fechado";
+    else
+        set msg_text = "O ticket já estava fechado";
+    end if;
+    call get_ticket_info(ticket_id);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `create_ticket` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -60,8 +96,9 @@ CREATE TABLE `ticket` (
 DELIMITER ;;
 CREATE DEFINER=`root`@`%` PROCEDURE `create_ticket`()
 BEGIN
-        INSERT INTO ticket (entry_date) VALUE(now());
-    END ;;
+    INSERT INTO ticket (entry_date) VALUE(now());
+    select "Ticket criado" as "msg";
+END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -78,27 +115,43 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`%` PROCEDURE `get_ticket_info`(IN ticket_id int)
-BEGIN
-    DECLARE entry_time datetime default now();
-    DECLARE entry_time_min int default 0;
-    DECLARE entry_time_hour decimal default 0;
-    DECLARE cost_p_hour INT default 0;
-    DECLARE no_pay INT default 0;
+label1:BEGIN
+    declare entry_time datetime;
+    declare entry_time_min int default 0;
+    declare entry_time_hour int default 0;
+    declare closing_time datetime;
+    declare cost_p_hour INT default 0;
+    declare no_pay INT default 0;
     declare cost int default 0;
 
+    #Checks if  the ticket exists
     SET entry_time= (SELECT entry_date FROM ticket WHERE id = ticket_id);
-    SET cost_p_hour= (SELECT sval FROM settings WHERE skey = 'cost-per-hour');
-    SET no_pay= (SELECT sval FROM settings WHERE skey = 'no_pay');
+    if(entry_time is null) then
+        #Does not exist
+        select "O ticket não existe" as "msg";
+        leave label1;
+    end if;
 
-    SET entry_time_min = timestampdiff(minute, entry_time, now());
+    SET cost_p_hour= (SELECT sval FROM settings WHERE skey = 'cost-per-hour');
+    SET no_pay= (SELECT sval FROM settings WHERE skey = 'no-pay');
+    
+    set closing_time = (select closing_date from ticket where id=ticket_id);
+    if(closing_time is null) then
+        SET entry_time_min = timestampdiff(minute, entry_time, now());
+    else
+        SET entry_time_min = timestampdiff(minute, entry_time, closing_time);
+    end if;
     SET entry_time_hour = entry_time_min / 60;
 
+    #Grace period
     if(entry_time_min > no_pay) then
         set cost = cost_p_hour * entry_time_hour;
     end if;
     
     select 
         ticket_id as "ticket",
+        entry_time as "data da entrada",
+        closing_time as "data de saída",
         cost as "total_a_pagar",
         entry_time_min as "tempo_estacionado_em_minutos",
         entry_time_hour as "tempo_estacionado_em_horas",
@@ -110,7 +163,7 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `open_ticket_list` */;
+/*!50003 DROP PROCEDURE IF EXISTS `get_ticket_list` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -120,7 +173,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`%` PROCEDURE `open_ticket_list`(list_size INT)
+CREATE DEFINER=`root`@`%` PROCEDURE `get_ticket_list`(IN list_size int)
 BEGIN
    SELECT id, entry_date, closing_date FROM ticket LIMIT list_size;
 END ;;
@@ -139,4 +192,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-08 19:04:13
+-- Dump completed on 2023-01-10 19:18:32
